2b9ada1711acbb9d89ab330d2356c01d69412ced
Hasan El Jabir <eljabirhasan@gmail.com>
2019-08-28 22:52:01 +0700

    Change Movie List from Activity to Fragment

---
+++ app/src/main/java/id/co/hasaneljabir/moviereview/feature/movie/MovieListFragment.java
@@ -0,0 +1,45 @@
+package id.co.hasaneljabir.moviereview.feature.movie;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import id.co.hasaneljabir.moviereview.R;
+import id.co.hasaneljabir.moviereview.adapter.MovieAdapter;
+import id.co.hasaneljabir.moviereview.model.movie.Movie;
+import id.co.hasaneljabir.moviereview.model.movie.MovieData;
+
+public class MovieListFragment extends Fragment {
+    private RecyclerView rvMovie;
+    private ArrayList<Movie> movieList = new ArrayList<>();
+
+    public MovieListFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        rvMovie = view.findViewById(R.id.rv_movie);
+        rvMovie.setHasFixedSize(true);
+
+        movieList.addAll(MovieData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvMovie.setLayoutManager(new LinearLayoutManager(getContext()));
+        MovieAdapter movieAdapter = new MovieAdapter(movieList);
+        rvMovie.setAdapter(movieAdapter);
+    }
+}
