deb59aeb85045a74d885e6570b0183dd4abdb6c5
Hasan El Jabir <eljabirhasan@gmail.com>
2019-08-28 23:49:33 +0700

    Add home activity

---
+++ app/src/main/java/id/co/hasaneljabir/moviereview/feature/HomeActivity.java
@@ -0,0 +1,72 @@
+package id.co.hasaneljabir.moviereview.feature;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import id.co.hasaneljabir.moviereview.R;
+import id.co.hasaneljabir.moviereview.feature.movie.MovieListFragment;
+import id.co.hasaneljabir.moviereview.feature.tvShow.TvShowListFragment;
+
+public class MovieTvShowActivity extends AppCompatActivity {
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_movie:
+                    fragment = new MovieListFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.navigation_tv_show:
+                    fragment = new TvShowListFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_tv_show);
+        setTitle(R.string.home);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        if (savedInstanceState == null) {
+            navigation.setSelectedItemId(R.id.navigation_movie);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_language_setting) {
+            Intent changeLanguage = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(changeLanguage);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
