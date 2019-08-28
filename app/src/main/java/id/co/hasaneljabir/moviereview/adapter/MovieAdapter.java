c799b0a3e5eec62750f51050940932addffc3e39
Hasan El Jabir <eljabirhasan@gmail.com>
2019-08-28 22:51:01 +0700

    Update Movie Adapter

--- app/src/main/java/id/co/hasaneljabir/moviereview/adapter/MovieAdapter.java
+++ app/src/main/java/id/co/hasaneljabir/moviereview/adapter/MovieAdapter.java
@@ -1,79 +1,64 @@
 package id.co.hasaneljabir.moviereview.adapter;
 
-import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 
 import id.co.hasaneljabir.moviereview.R;
-import id.co.hasaneljabir.moviereview.model.Movie;
+import id.co.hasaneljabir.moviereview.model.movie.Movie;
 
-public class MovieAdapter extends BaseAdapter {
-    private Context context;
-    private ArrayList<Movie> movies;
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private ArrayList<Movie> listMovie;
 
-    public void setMovie(ArrayList<Movie> movies) {
-        this.movies = movies;
+    public MovieAdapter(ArrayList<Movie> list) {
+        this.listMovie = list;
     }
 
-    public MovieAdapter(Context context) {
-        this.context = context;
-        movies = new ArrayList<>();
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_movie, viewGroup, false);
+        return new MovieViewHolder(view);
     }
 
     @Override
-    public int getCount() {
-        return movies.size();
+    public void onBindViewHolder(@NonNull final MovieViewHolder holder, int position) {
+        Movie movie = listMovie.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(movie.getPoster())
+                .apply(new RequestOptions().override(350, 550))
+                .into(holder.ivPoster);
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvReleaseDate.setText(movie.getReleaseDate());
+        holder.tvSynopsis.setText(movie.getSynopsis());
     }
 
     @Override
-    public Object getItem(int i) {
-        return movies.get(i);
+    public int getItemCount() {
+        return listMovie.size();
     }
 
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvReleaseDate, tvSynopsis;
+        ImageView ivPoster;
 
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.item_movie, viewGroup, false);
-        }
+        MovieViewHolder(View itemView) {
+            super(itemView);
 
-        ViewHolder viewHolder = new ViewHolder(view);
-        Movie movie = (Movie) getItem(i);
-        viewHolder.bind(movie, view);
-        return view;
-    }
-
-    private class ViewHolder {
-        private TextView tvTitle, tvReleaseDate, tvSynopsis;
-        private ImageView ivPoster;
-
-        ViewHolder(View view) {
-            tvTitle = view.findViewById(R.id.tv_title);
-            tvReleaseDate = view.findViewById(R.id.tv_release_date);
-            tvSynopsis = view.findViewById(R.id.tv_synopsis);
-            ivPoster = view.findViewById(R.id.iv_poster);
-        }
-
-        void bind(Movie movie, View view) {
-            tvTitle.setText(movie.getTitle());
-            tvReleaseDate.setText(movie.getReleaseDate());
-            tvSynopsis.setText(movie.getSynopsis());
-
-            Glide.with(view.getContext())
-                    .load(movie.getPoster())
-                    .into(ivPoster);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            tvReleaseDate = itemView.findViewById(R.id.tv_release_date);
+            tvSynopsis = itemView.findViewById(R.id.tv_synopsis);
+            ivPoster = itemView.findViewById(R.id.iv_poster);
         }
     }
 }
+
