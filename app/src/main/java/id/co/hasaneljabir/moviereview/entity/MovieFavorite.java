5b995870477e022265ce8ede2aa9f085d07fbf47
Hasan El Jabir <eljabirhasan@gmail.com>
2019-09-12 13:49:13 +0700

    Add Room table for favorite Movie and TV Show

---
+++ app/src/main/java/id/co/hasaneljabir/moviereview/entity/MovieFavorite.java
@@ -0,0 +1,74 @@
+package id.co.hasaneljabir.moviereview.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "movie_favorite")
+public class MovieFavorite {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "releaseDate")
+    private String releaseDate;
+
+    @ColumnInfo(name = "voteAverage")
+    private String voteAverage;
+
+    @ColumnInfo(name = "overview")
+    private String overview;
+
+    @ColumnInfo(name = "posterPath")
+    private String posterPath;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+}
