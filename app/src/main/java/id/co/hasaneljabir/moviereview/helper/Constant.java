52f6cdd8e472ba2b60c69fabdcb21bfdb24c48d1
Hasan El Jabir <eljabirhasan@gmail.com>
2019-09-25 11:50:41 +0700

    Add helper for Constant values

---
+++ app/src/main/java/id/co/hasaneljabir/moviereview/helper/Constant.java
@@ -0,0 +1,6 @@
+package id.co.hasaneljabir.moviereview.helper;
+
+public class Constant {
+    public static String POSTER_BASE_URL_185 = "https://image.tmdb.org/t/p/w185";
+    public static String POSTER_BASE_URL_342 = "https://image.tmdb.org/t/p/w342";
+}
