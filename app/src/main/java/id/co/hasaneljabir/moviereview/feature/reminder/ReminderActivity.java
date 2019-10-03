6fd99a83f37a79f1f3c3b90997d8f580ef2b36a3
Hasan El Jabir <eljabirhasan@gmail.com>
2019-10-03 12:57:39 +0700

    Fix reminder feature

--- app/src/main/java/id/co/hasaneljabir/moviereview/feature/reminder/ReminderActivity.java
+++ app/src/main/java/id/co/hasaneljabir/moviereview/feature/reminder/ReminderActivity.java
@@ -1,30 +1,16 @@
 package id.co.hasaneljabir.moviereview.feature.reminder;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
-import org.json.JSONObject;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import cz.msebera.android.httpclient.Header;
-import id.co.hasaneljabir.moviereview.BuildConfig;
 import id.co.hasaneljabir.moviereview.R;
 import id.co.hasaneljabir.moviereview.sharedPreference.SharedPreference;
 
 public class ReminderActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String API_KEY = BuildConfig.TMDB_API_KEY;
     private SharedPreference sharedPreference;
 
     private ReminderReceiver reminderReceiver;
@@ -94,40 +80,5 @@ public class ReminderActivity extends AppCompatActivity implements View.OnClickL
         } else {
             switchNewRelease.setChecked(false);
         }
-    }
-
-    public static void getNewRelease(final Context context) {
-        final SharedPreference sharedPreference = new SharedPreference(context);
-
-        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        String currentDate = date.format(new Date());
-
-        final AsyncHttpClient[] client = {new AsyncHttpClient()};
-        String url = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&primary_release_date.gte="
-                + currentDate + "&primary_release_date.lte=" + currentDate;
-
-        client[0].get(url, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                try {
-                    String result = new String(responseBody);
-                    JSONObject responseObject = new JSONObject(result);
-
-                    String title = responseObject.getJSONArray("results").getJSONObject(0).getString("title");
-                    sharedPreference.saveString(SharedPreference.MOVIE_TITLE, title);
-
-                    Toast.makeText(context, sharedPreference.getNewReleaseMovieTitle(), Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
-                    sharedPreference.saveString(SharedPreference.MOVIE_TITLE, "Error Response");
-                    Log.d("movieNewReleaseErr", e.getMessage());
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                sharedPreference.saveString(SharedPreference.MOVIE_TITLE, "No Internet Connection");
-                Log.d("movieNewReleaseNoConn", error.getMessage());
-            }
-        });
     }
 }
--- app/src/main/java/id/co/hasaneljabir/moviereview/feature/reminder/ReminderReceiver.java
+++ app/src/main/java/id/co/hasaneljabir/moviereview/feature/reminder/ReminderReceiver.java
@@ -13,37 +13,44 @@ import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
+import cz.msebera.android.httpclient.Header;
+import id.co.hasaneljabir.moviereview.BuildConfig;
 import id.co.hasaneljabir.moviereview.R;
-import id.co.hasaneljabir.moviereview.sharedPreference.SharedPreference;
 
 public class ReminderReceiver extends BroadcastReceiver {
+    private static final String API_KEY = BuildConfig.TMDB_API_KEY;
     public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+    private String title, message;
 
     public ReminderReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        SharedPreference sharedPreference = new SharedPreference(context);
+        message = intent.getStringExtra(EXTRA_MESSAGE);
 
-        String message = intent.getStringExtra(EXTRA_MESSAGE);
-        String title = "Movie Review";
-
-        if (message.equalsIgnoreCase("EXTRA_MESSAGE")) {
-            ReminderActivity.getNewRelease(context);
-
-            title = "New Released Movie";
-            message = sharedPreference.getNewReleaseMovieTitle();
+        if (message.equalsIgnoreCase("EXTRA_MESSAGE")) { //New released movie reminder
+            getNewRelease(context);
+        } else { //Daily reminder
+            title = "Movie Review";
+            showReminderNotification(context, title, message);
         }
 
-        showReminderNotification(context, title, message);
     }
 
     public boolean isDateInvalid(String date, String format) {
@@ -158,5 +165,43 @@ public class ReminderReceiver extends BroadcastReceiver {
         if (alarmManager != null) {
             alarmManager.cancel(pendingIntent);
         }
+    }
+
+    private void getNewRelease(final Context context) {
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String currentDate = date.format(new Date());
+
+        String url = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&primary_release_date.gte="
+                + currentDate + "&primary_release_date.lte=" + currentDate;
+
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject responseObject = new JSONObject(result);
+                    JSONArray list = responseObject.getJSONArray("results");
+
+                    title = "New Released Movie";
+                    message = "";
+
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject releasedMovie = list.getJSONObject(i);
+                        message = message + releasedMovie.getString("title") + ", ";
+                    }
+
+                    showReminderNotification(context, title, message);
+                } catch (Exception e) {
+                    Log.d("movieNewReleaseErr", e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("movieNewReleaseNoConn", error.getMessage());
+            }
+        });
     }
 }
